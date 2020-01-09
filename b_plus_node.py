505962b575862bbfd6a9f58fbe3f2ddbb3db4ed7
Deepak R <deepakr@Deepaks-MacBook-Pro.local>
2020-01-09 07:35:25 +0530

    Fixed all linting issues

--- b_plus_node.py
+++ b_plus_node.py
@@ -9,7 +9,7 @@ class Leaf:
         self.parent = parent
         self.branching_factor = branching_factor
         self.keys = []
-        self.children = [] # (key, value)
+        self.children = []  # (key, value)
 
     def set(self, key, value):
         index = bisect.bisect_left(self.keys, key)
@@ -28,14 +28,19 @@ class Leaf:
 
     def split(self, index):
         self.next = Leaf(self, self.next, self.parent, self.branching_factor)
-        
         self.next.keys = self.keys[index:]
         self.next.children = self.children[index:]
         self.keys = self.keys[:index]
         self.children = self.keys[:index]
         # bubble up
         if self.is_root():
-            self.parent = Node(None, None, [self.next.keys[0]], [self, self.next], branching_factor=self.branching_factor)
+            self.parent = Node(
+                None,
+                None,
+                [self.next.keys[0]],
+                [self, self.next],
+                branching_factor=self.branching_factor
+            )
             self.next.parent = self.parent
         else:
             self.parent.add_child(self.next.keys[0], self.next)
@@ -82,8 +87,8 @@ class Node:
     def __init__(self, previous_node, next_node, keys, children, parent=None, branching_factor=16):
         self.previous = previous_node
         self.next = next_node
-        self.keys = keys # NOTE: must keep keys sorted
-        self.children = children # NOTE: children must correspond to parents.
+        self.keys = keys  # NOTE: must keep keys sorted
+        self.children = children  # NOTE: children must correspond to parents.
         self.parent = parent
         self.branching_factor = branching_factor
         for child in children:
@@ -98,43 +103,54 @@ class Node:
         self.children[i + 1].set(key, value)
 
 
-    def get(self, key):
-        # TODO: speed up finding the right bucket
-        for i, k in enumerate(self.keys):
-            if key < k:
-                return self.children[i].get(key)
-        return self.children[i + 1].get(key)
+def get(self, key):
+    # TODO: speed up finding the right bucket
+    for i, k in enumerate(self.keys):
+        if key < k:
+            return self.children[i].get(key)
+    return self.children[i + 1].get(key)
 
 
-    def add_child(self, key, greater_child):
-        # Childs keys must all be greater than key
-        index = bisect.bisect(self.keys, key)
-        self.keys.insert(index, key)
-        self.children.insert(index + 1, greater_child)
-        # Bubble up if too many children
-        if len(self.keys) == self.branching_factor:
-            self.split(self.branching_factor // 2)
+def add_child(self, key, greater_child):
+    # Childs keys must all be greater than key
+    index = bisect.bisect(self.keys, key)
+    self.keys.insert(index, key)
+    self.children.insert(index + 1, greater_child)
+    # Bubble up if too many children
+    if len(self.keys) == self.branching_factor:
+        self.split(self.branching_factor // 2)
 
 
-    def change_key(self, old_key, new_key):
-        """Replaces the first key that is greater or equal than
-        old_key with new_key or modifies the parent's key so that new_key
-        falls within the current node"""
-        if new_key < self.keys[0]:
-            self.parent.change_key(self.keys[0], new_key)
-        for i, k in enumerate(self.keys):
-            if k >= old_key:
-                self.keys[i] = new_key
+def change_key(self, old_key, new_key):
+    """Replaces the first key that is greater or equal than
+    old_key with new_key or modifies the parent's key so that new_key
+    falls within the current node"""
+    if new_key < self.keys[0]:
+        self.parent.change_key(self.keys[0], new_key)
+    for i, k in enumerate(self.keys):
+        if k >= old_key:
+            self.keys[i] = new_key
 
     def split(self, index):
         # Sibling has keys greater than the current
-        self.next = Node(self, self.next, self.keys[index + 1:], self.children[index + 1:], self.parent)
+        self.next = Node(
+            self, self.next,
+            self.keys[index + 1:],
+            self.children[index + 1:],
+            self.parent
+        )
         split_key = self.keys[index]
         self.keys = self.keys[:index]
         self.children = self.children[:index + 1]
 
         if self.is_root():
-            self.parent = Node(None, None, [split_key], [self, self.next], branching_factor=self.branching_factor)
+            self.parent = Node(
+                None,
+                None,
+                [split_key],
+                [self, self.next],
+                branching_factor=self.branching_factor
+            )
         else:
             self.parent.add_child(split_key, self.next)
         return self.next
@@ -169,7 +185,7 @@ class Node:
     def balance(self):
         # Borrow from siblings if necessary
         if not self.is_root() and self.size() < self.branching_factor // 2:
-            if self.previous is not None and self.previous.size() > self.branching_factor // 2:
+            if ((self.previous is not None) and (self.previous.size() > self.branching_factor // 2)):
                 self.keys.insert(0, self.previous.keys.pop(-1))
                 self.children.insert(0, self.previous.children.pop(-1))
                 self.parent.change_key(self.keys[0], self.keys[0])
